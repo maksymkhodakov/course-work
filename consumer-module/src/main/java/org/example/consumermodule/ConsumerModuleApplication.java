09474af1117c34966013a77e44e61963b54f6f2d
Jovakinn <max05012004@gmail.com>
2024-01-29 01:02:49 +0200

    add kafka producer and start consumer

--- consumer-module/src/main/java/org/example/consumermodule/ConsumerModuleApplication.java
+++ consumer-module/src/main/java/org/example/consumermodule/ConsumerModuleApplication.java
@@ -4,10 +4,8 @@ import org.example.producermodule.ProducerModuleApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
-import org.springframework.kafka.annotation.EnableKafka;
 
 @Import(ProducerModuleApplication.class)
-@EnableKafka
 @SpringBootApplication
 public class ConsumerModuleApplication {
 
--- producer-module/src/main/java/org/example/producermodule/ProducerModuleApplication.java
+++ producer-module/src/main/java/org/example/producermodule/ProducerModuleApplication.java
@@ -2,9 +2,7 @@ package org.example.producermodule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.EnableKafka;
 
-@EnableKafka
 @SpringBootApplication
 public class ProducerModuleApplication {
 
