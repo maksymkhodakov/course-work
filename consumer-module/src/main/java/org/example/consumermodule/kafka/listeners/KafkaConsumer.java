2fdcdaf3dec3a7df11a7c46ddc08739449987014
Jovakinn <max05012004@gmail.com>
2024-01-30 02:07:30 +0200

    add kafka producer and start consumer

--- consumer-module/src/main/java/org/example/consumermodule/kafka/listeners/KafkaConsumer.java
+++ consumer-module/src/main/java/org/example/consumermodule/kafka/listeners/KafkaConsumer.java
@@ -1,12 +1,8 @@
 package org.example.consumermodule.kafka.listeners;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.example.consumermodule.domain.entity.AnimalStream;
-import org.example.consumermodule.domain.repositories.AnimalRepository;
-import org.example.producermodule.domain.dto.AnimalDTO;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,25 +11,10 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class KafkaConsumer {
-    private final ObjectMapper objectMapper;
-    private final AnimalRepository animalRepository;
 
     @Transactional
     @KafkaListener(topics = "${kafka.topics.dev}", groupId = "${spring.kafka.consumer.group-id}")
     public void listen(ConsumerRecord<String, String> message) {
-        log.info("Consumer received message for topic: {}", message.topic());
-        readMessage(objectMapper.convertValue(message.value(), AnimalDTO.class));
-    }
-
-
-    public void readMessage(AnimalDTO message) {
-        final AnimalStream animalStream = AnimalStream.builder()
-                .name(message.getName())
-                .age(message.getAge())
-                .venomous(message.isVenomous())
-                .build();
-        final AnimalStream savedAnimalStream = animalRepository.save(animalStream);
-
-        log.info("Animal saved with id={}", savedAnimalStream.getId());
+        log.info("Consumer received message for topic: {}, value: {}", message.topic(), message.value());
     }
 }
