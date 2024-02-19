c7abc7b2ec382b60ebdf2813ed10337c2e8aa2bf
Jovakinn <max05012004@gmail.com>
2024-02-19 23:07:13 +0200

    add producer module endpoint authorization

--- producer-module/src/main/java/org/example/producermodule/kafka/api/KafkaController.java
+++ producer-module/src/main/java/org/example/producermodule/kafka/api/KafkaController.java
@@ -9,6 +9,7 @@ import org.example.producermodule.dto.AnimalUpdateDTO;
 import org.example.producermodule.kafka.config.TopicNameConfig;
 import org.example.producermodule.kafka.service.KafkaSenderService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -20,18 +21,21 @@ public class KafkaController {
     private final KafkaSenderService kafkaSenderService;
 
     @PostMapping("/kafka/produce/animal/save")
+    @PreAuthorize("hasPermission(null, T(com.example.zoo.enums.Privilege).ROLE_BASIC_USER)")
     public ResponseEntity<Void> produceCreate(@RequestBody @Valid AnimalDTO animalDTO) {
         kafkaSenderService.produceMessages(topicNameConfig.getDevSave(), animalDTO);
         return ResponseEntity.ok().build();
     }
 
     @PutMapping("/kafka/produce/animal/update")
+    @PreAuthorize("hasPermission(null, T(com.example.zoo.enums.Privilege).ROLE_BASIC_USER)")
     public ResponseEntity<Void> produceUpdate(@RequestBody @Valid AnimalUpdateDTO animalUpdateDTO) {
         kafkaSenderService.produceMessages(topicNameConfig.getDevUpdate(), animalUpdateDTO);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/kafka/produce/animal/delete")
+    @PreAuthorize("hasPermission(null, T(com.example.zoo.enums.Privilege).ROLE_BASIC_USER)")
     public ResponseEntity<Void> produceDelete(@RequestBody @Valid AnimalDeleteDTO animalDeleteDTO) {
         kafkaSenderService.produceMessages(topicNameConfig.getDevDelete(), animalDeleteDTO);
         return ResponseEntity.ok().build();
--- producer-module/src/main/java/org/example/producermodule/rabbitmq/api/RabbitMqController.java
+++ producer-module/src/main/java/org/example/producermodule/rabbitmq/api/RabbitMqController.java
@@ -8,6 +8,7 @@ import org.example.producermodule.dto.AnimalDeleteDTO;
 import org.example.producermodule.dto.AnimalUpdateDTO;
 import org.example.producermodule.rabbitmq.service.MessageProducerWrapper;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -19,18 +20,21 @@ public class RabbitMqController {
     private final MessageProducerWrapper messageProducerWrapper;
 
     @PostMapping("/rabbitmq/produce/animal/save")
+    @PreAuthorize("hasPermission(null, T(com.example.zoo.enums.Privilege).ROLE_BASIC_USER)")
     public ResponseEntity<Void> produceCreate(@RequestBody @Valid AnimalDTO animalDTO) {
         messageProducerWrapper.produceDevMessages(animalDTO);
         return ResponseEntity.ok().build();
     }
 
     @PutMapping("/rabbitmq/produce/animal/update")
+    @PreAuthorize("hasPermission(null, T(com.example.zoo.enums.Privilege).ROLE_BASIC_USER)")
     public ResponseEntity<Void> produceUpdate(@RequestBody @Valid AnimalUpdateDTO animalUpdateDTO) {
         messageProducerWrapper.produceDevMessages(animalUpdateDTO);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/rabbitmq/produce/animal/delete")
+    @PreAuthorize("hasPermission(null, T(com.example.zoo.enums.Privilege).ROLE_BASIC_USER)")
     public ResponseEntity<Void> produceDelete(@RequestBody @Valid AnimalDeleteDTO animalDeleteDTO) {
         messageProducerWrapper.produceDevMessages(animalDeleteDTO);
         return ResponseEntity.ok().build();
